018fff0e1f890dfafded4aa81ea5e07b9197e1f5
student <student@Maclab-S001.local>
2016-10-19 12:29:30 +0800

    Initial Commit

---
+++ GUESS THE NUMBERRRRRRRTests/GUESS_THE_NUMBERRRRRRRTests.swift
@@ -0,0 +1,36 @@
+//
+//  GUESS_THE_NUMBERRRRRRRTests.swift
+//  GUESS THE NUMBERRRRRRRTests
+//
+//  Created by student on 19/10/2016.
+//  Copyright © 2016 student. All rights reserved.
+//
+
+import XCTest
+@testable import GUESS_THE_NUMBERRRRRRR
+
+class GUESS_THE_NUMBERRRRRRRTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
